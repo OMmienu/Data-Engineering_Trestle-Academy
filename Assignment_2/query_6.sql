548713a53af89171c51cdabc50fc4a57d0fa9919
OMmienu <nkedzi@gmail.com>
2024-11-08 04:06:19 -0800

    query_6.sql

---
+++ Assignment_2/query_6.sql
@@ -0,0 +1,2 @@
+Query 6 'Multi-Table Join with Conditions'
+Retrieve the names of students along with the subjects they are enrolled in and the teachers name for each subject. This requires joining multiple tables: 'Students, Enrollments, Subjects and Teachers'.
