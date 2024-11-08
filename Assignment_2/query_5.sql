aae5bbb9ea9b1d6f7989a2ae46a79d1c5f9fe52d
OMmienu <nkedzi@gmail.com>
2024-11-08 04:01:08 -0800

    query_5.sql

---
+++ Assignment_2/query_5.sql
@@ -0,0 +1,2 @@
+Query 5 'Joining Tables'
+Retrieve the first and last names of students along with the names of the classes they are enrolled in. Use 'JOIN' to connect the Students and Classes tables.
